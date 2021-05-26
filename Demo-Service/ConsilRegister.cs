820cacd0b63961caf99910088c2970312930c8b6
zhenwu hu <463372303@qq.com>
2021-05-26 10:02:01 +0800

    Add project files.

---
+++ Demo-Service/ConsilRegister.cs
@@ -0,0 +1,42 @@
+﻿using Consul;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_Service
+{
+    public static class ConsulHelper
+    {
+        public static void ConsulRegist(this IConfiguration configuration)
+        {
+            ConsulClient client = new ConsulClient(c =>
+            {
+                c.Address = new Uri(configuration.GetValue<string>("ConsolHost"));
+                c.Datacenter = "dcl";
+            });
+
+            string ip = configuration.GetValue<string>("LocalIp");
+            int port = configuration.GetValue<int>("LocalPort");
+            var http = $"http://{ip}:{port}/student";
+            Console.WriteLine(http);
+
+
+            client.Agent.ServiceRegister(new AgentServiceRegistration()
+            {
+                ID = "StudentService" + Guid.NewGuid(),
+                Name = "StudentService",
+                Address = ip,
+                Port = port,
+                Check = new AgentServiceCheck()
+                {
+                    Interval = TimeSpan.FromSeconds(12),
+                    HTTP = http,
+                    Timeout = TimeSpan.FromSeconds(5),
+                    DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(60)
+                }
+            });
+        }
+    }
+}
---
+++ Demo-Service/Controllers/WeatherForecastController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly ILogger<StudentController> _logger;
+
+        public StudentController(ILogger<StudentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("name")]
+        public string Name()
+        {
+
+            return $"my name is {Guid.NewGuid()}, current port is:{ Request.Host.Port}";
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+
+            return $"my name is {Guid.NewGuid()}, current port is:{ Request.Host.Port}";
+        }
+    }
+}
