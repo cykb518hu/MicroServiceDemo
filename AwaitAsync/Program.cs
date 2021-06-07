47ac1636d3ff24eb58b8d63774793f2e0ba731d5
zhenwu hu <463372303@qq.com>
2021-06-07 09:19:05 +0800

    update

--- AwaitAsync/Program.cs
+++ AwaitAsync/Program.cs
@@ -28,10 +28,11 @@ namespace AwaitAsync
     /// </summary>
     public class AwaitAsyncClass
     {
-        public void Show()
+        public async Task  Show()
         {
             Console.WriteLine($"this Main start {Thread.CurrentThread.ManagedThreadId}");
-            NoReturnTask();
+            await AsyncMethod();
+            Thread.Sleep(1000);
             Console.WriteLine($"this Main end {Thread.CurrentThread.ManagedThreadId}");
         }
 
@@ -88,6 +89,27 @@ namespace AwaitAsync
             {
                 Console.WriteLine("**********");
             });
+        }
+
+        private async Task AsyncMethod()
+        {
+            var ResultFromTimeConsumingMethod = TimeConsumingMethod();
+            string Result = await ResultFromTimeConsumingMethod + " + AsyncMethod. My Thread ID is :" + Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine(Result);
+            //返回值是Task的函数可以不用return
+        }
+
+        //这个函数就是一个耗时函数，可能是IO操作，也可能是cpu密集型工作。
+        private Task<string> TimeConsumingMethod()
+        {
+            var task = Task.Run(() => {
+                Console.WriteLine("Helo I am TimeConsumingMethod. My Thread ID is :" + Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(2000);
+                Console.WriteLine("Helo I am TimeConsumingMethod after Sleep(5000). My Thread ID is :" + Thread.CurrentThread.ManagedThreadId);
+                return "Hello I am TimeConsumingMethod";
+            });
+
+            return task;
         }
     }
 }
